77a0d183b7432a231a6b1223da32e328ce4983ff
alexzoo0 <alexisvelasquez15@gmail.com>
2021-09-20 21:48:26 -0500

    added variables and clickStart function

--- assets/js/json.js
+++ assets/js/json.js
@@ -2,5 +2,18 @@ var questions = document.querySelector("#container");
 var answers = document.querySelector("#answer");
 var startBtn = document.querySelector("#starter");
 var enterBtn = document.querySelector("#enter");
-var time = document.querySelector("#time");
+var times = document.querySelector("#time");
 var initials = document.querySelector("#init");
+
+function clickStart(){
+    var bScreen = document.getElementById("start");
+    bScreen.setAttribute("class", "hide");
+
+    questions.removeAttribute("class");
+
+    var time = setInterval(2000);
+    var timer = questions.length * 10;
+    time.textContent = timer;
+
+    questionsB()
+}
